a0f73cd3a72820fb1676a86947570b4a91792a40
Sreetama <fool100abc@gmail.com>
2023-09-14 09:32:04 +0530

    Few changes added

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This repo monitors real time cardiac activities of a person through remote photo
 ### Pre Processing 
 Skin pixels play significant role in extraction of rPPG signal therefore, we trained first ever deep learning model for semantic 
 segmentation of skin and non skin pixels. This is novel technique for region of interst (ROI) selection and tracking. The model is robust to motion, multiple postures and segments skin pixels from non skin very accurately.
-rPPG signal exhibit different waveform when sampled from different rigions of skin, therefore, to consistently sample ROI from same part of skin we detect face as prerequisite step to semantic segmentation.
+rPPG signal exhibit different waveform when sampled from different regions of skin, therefore, to consistently sample ROI from same part of skin we detect face as prerequisite step to semantic segmentation.
 
 ### rPPG Signal Extraction 
 After detection and tracking ROI for signal extraction we compute the spatial red, green and blue channel mean of skin segmented pixels to minimise camera quantization error. Averaged values of RGB channel are temporally normalized and projected to plane orthogonal to skin-tone. The projected signal is alpha tuned to extract signal. 
--- run.py
+++ run.py
@@ -9,28 +9,31 @@ from optparse import OptionParser
 
 class RunPOS():
     def __init__(self,  sz=270, fs=28, bs=30, plot=False):
-        self.batch_size = bs
+        self.batch_size = bs 
         self.frame_rate = fs
         self.signal_size = sz
         self.plot = plot
 
     def __call__(self, source):
         time1=time.time()
-        
-        mask_process_pipe, chil_process_pipe = mp.Pipe()
-        self.plot_pipe = None
+
+        mask_process_pipe, chil_process_pipe = mp.Pipe() # multiprocessing 
+
+        self.plot_pipe = None 
         if self.plot:
-            self.plot_pipe, plotter_pipe = mp.Pipe()
+            self.plot_pipe, plotter_pipe = mp.Pipe() # multiprocessing
             self.plotter = DynamicPlot(self.signal_size, self.batch_size)
-            self.plot_process = mp.Process(target=self.plotter, args=(plotter_pipe,), daemon=True)
+            self.plot_process = mp.Process(target=self.plotter, args=(plotter_pipe,), daemon=True) # multiprocessing
             self.plot_process.start()
         
         process_mask = ProcessMasks(self.signal_size, self.frame_rate, self.batch_size)
 
-        mask_processer = mp.Process(target=process_mask, args=(chil_process_pipe, self.plot_pipe, source, ), daemon=True)
+        mask_processer = mp.Process(target=process_mask, args=(chil_process_pipe, self.plot_pipe, source, ), daemon=True)  # multiprocessing
+
         mask_processer.start()
         
-        capture = CaptureFrames(self.batch_size, source, show_mask=True)
+        capture = CaptureFrames(self.batch_size, source, show_mask=True) 
+
         capture(mask_process_pipe, source)
 
         mask_processer.join()
